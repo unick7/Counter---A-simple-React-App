56335a66c86f49decc6a8f335c12a3e2745c0218
Nikhil Verma <lci2020044@iiitl.ac.in>
2022-09-03 23:57:20 +0530

    Update App.js
    Implemented counter functionality

--- counter-app/src/App.js
+++ counter-app/src/App.js
@@ -1,9 +1,14 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [counter, setCounter] = useState(0);
   return (
     <div className="App">
-      <counter></counter>
+      <button onClick={() => setCounter((prevCounter) => prevCounter - 1)}>-</button>
+      <h1>{counter}</h1>
+      <button onClick={() => setCounter((prevCounter) => prevCounter + 1)}>+</button>
     </div>
   );
 }
